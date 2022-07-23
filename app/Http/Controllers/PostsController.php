9ccbe143d817508c249146afa4679e7baa763c25
oliver <kunhan945@gmail.com>
2022-07-23 20:34:18 +0800

    resource controller

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    private $posts = [
+        1 => [
+            'title' => 'Laravel',
+            'content' => 'Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller (MVC) architectural pattern.',
+            'is_new' => true,
+        ],
+        2 => [
+            'title' => 'Vue.js',
+            'content' => 'Vue.js is a progressive framework for building user interfaces. It is designed from the ground up to be incrementally adoptable.',
+            'is_new' => false,
+        ],
+    ];
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('posts.index', ['posts' => $this->posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return  view('posts.show', ['post' => $this->posts[$id]]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -31,32 +32,35 @@ Route::get('/contact', [HomeController::class, 'contact'])
     ->name('home.contact');
 Route::get('/single', AboutController::class);
 
+Route::resource('posts', PostsController::class)
+    ->only(['index', 'show']);
+
 //Route::get('/contact', function () {
 //    return view('home.contact');
 //})->name('home.contact');
 
 //Route::view('/contact', 'home.contact');
 
-Route::get('/posts', function (Request $request) use ($posts) {
-    //dd(request()->all());
-    //dd((int)request()->input('page', 1));
-    //dd((int)request()->query('page', 1));
+//Route::get('/posts', function (Request $request) use ($posts) {
+//    //dd(request()->all());
+//    //dd((int)request()->input('page', 1));
+//    //dd((int)request()->query('page', 1));
 
-    return view('posts.index', ['posts' => $posts]);
-});
+//    return view('posts.index', ['posts' => $posts]);
+//});
 
-Route::get('/posts/{id}', function ($id) use ($posts) {
-    abort_if(!isset($posts[$id]), 404);
-    return  view('posts.show', ['post' => $posts[$id]]);
-})
-    //->where([
-    //    'id' => '[0-9]+',
-    //]) // handled with RouteServiceProvider.php boot() -> Route::pattern('id', '[0-9]+');
-    ->name('posts.show');
+//Route::get('/posts/{id}', function ($id) use ($posts) {
+//    abort_if(!isset($posts[$id]), 404);
+//    return  view('posts.show', ['post' => $posts[$id]]);
+//})
+//    //->where([
+//    //    'id' => '[0-9]+',
+//    //]) // handled with RouteServiceProvider.php boot() -> Route::pattern('id', '[0-9]+');
+//    ->name('posts.show');
 
-Route::get('/recent-posts/{days_ago?}', function ($days_ago = 20) {
-    return 'Posts from ' . $days_ago . ' days ago';
-})->name('posts.recent.index')->middleware('auth');
+//Route::get('/recent-posts/{days_ago?}', function ($days_ago = 20) {
+//    return 'Posts from ' . $days_ago . ' days ago';
+//})->name('posts.recent.index')->middleware('auth');
 
 Route::prefix('/fun')->name('fun.')->group(function () use ($posts) {
 
